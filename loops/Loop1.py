b065c53c1f88bdd5419bec29c937ae3e0dcdb6b5
gfarias83 <gabe.farias.83@gmail.com>
2017-03-11 13:14:29 -0500

    python

---
+++ loops/Loop1.py
@@ -0,0 +1,10 @@
+count = 0
+while (count < 10):
+    print ("count is") , count
+    count = count + 1
+
+print ("done")
+
+
+    
+    
---
+++ loops/Loop2.py
@@ -0,0 +1,5 @@
+for x in range (50,0,- 5):
+    print (x) 
+'''Loser'''
+
+
